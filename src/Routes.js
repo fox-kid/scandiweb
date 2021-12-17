587c4851acb90c0345cfff9617d6a20439a97a2c
fox-kid <alezh15@freeuni.edu.ge>
2021-12-17 16:03:32 +0400

    Beginning of project

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Header from "./components/Header";
+
+class Routes extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li>asd</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
